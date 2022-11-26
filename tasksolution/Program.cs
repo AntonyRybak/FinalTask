e658506b7eea8f92ff3e2a787cc63a93cae0340c
Anton Rybak <toxa-wow@mail.ru>
2022-11-26 20:26:40 +0300

    firstmethod

--- tasksolution/Program.cs
+++ tasksolution/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿Console.Clear();
+string[] str = new string[] {"hello", "2", "world", ":-)"};
+string[] result = new string[str.Length];
+
+void Arraytransform(string[]str, string[]result);
+{
+    int count = 0;
+    for (int i = 0; i < str.Length; i++)
+    {
+        if (str[i].Length <= 3)
+        {
+    }
+    result[count] = str[i];
+    count++;
+}
+}
